1d646ad3a541ddd350369badff03f86515944078
IvanOvcharuk <ivanovcharuk228@gmail.com>
2022-12-20 18:55:36 +0100

    sortowanie szybkie

--- app/src/main/java/com/example/zadanie_sortowanie_ovcharuk/MainActivity.kt
+++ app/src/main/java/com/example/zadanie_sortowanie_ovcharuk/MainActivity.kt
@@ -55,6 +55,32 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        //Szybkie
+        fun partition(array: IntArray, low: Int, high: Int): Int {
+            val pivot = array[high]
+            var i = low - 1
+            for (j in low until high) {
+                if (array[j] <= pivot) {
+                    i++
+                    val temp = array[i]
+                    array[i] = array[j]
+                    array[j] = temp
+                }
+            }
+            val temp = array[i + 1]
+            array[i + 1] = array[high]
+            array[high] = temp
+            return i + 1
+        }
+
+        fun szybkie(array: MutableList<Int>, low: Int, high: Int) {
+            if (low < high) {
+                val pivot = partition(array.toIntArray(), low, high)
+                szybkie(array, low, pivot - 1)
+                szybkie(array, pivot + 1, high)
+            }
+        }
+
 
     }
 }
