244196ceb13bcc09bd2cbc84f969b9c382c4a4ae
IvanOvcharuk <ivanovcharuk228@gmail.com>
2022-12-20 18:56:38 +0100

    sortowanie scalanie

--- app/src/main/java/com/example/zadanie_sortowanie_ovcharuk/MainActivity.kt
+++ app/src/main/java/com/example/zadanie_sortowanie_ovcharuk/MainActivity.kt
@@ -121,6 +121,44 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+        //Scalanie
+        fun mergeHalvesIA(workA: IntArray,
+                          workB: IntArray,
+                          start: Int,
+                          mid: Int,
+                          exclusiveEnd: Int) {
+            var p1 = start
+            var p2 = mid
+            for (i in start until exclusiveEnd) {
+                if (p1 < mid && (p2 == exclusiveEnd || workA[p1] <= workA[p2])) {
+                    workB[i] = workA[p1]
+                    p1++
+                } else {
+                    workB[i] = workA[p2]
+                    p2++
+                }
+            }
+        }
+        fun sortSectionIA(input: IntArray,
+                          output: IntArray,
+                          start: Int,
+                          exclusiveEnd: Int) : Unit {
+
+            if (exclusiveEnd - start <= 1)
+                return
+            val mid = (start + exclusiveEnd) / 2
+            sortSectionIA(output, input, start, mid)
+            sortSectionIA(output, input, mid, exclusiveEnd)
+            mergeHalvesIA(input, output, start, mid, exclusiveEnd)
+        }
+        fun mergeSortIA(array: IntArray) : IntArray {
+            val arrayCopy = array.copyOf()
+            val arrayCopy2 = array.copyOf()
+
+            sortSectionIA(arrayCopy, arrayCopy2, 0, arrayCopy.size)
+
+            return arrayCopy2
+        }
 
     }
 }
